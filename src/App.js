74f93eef07c3dd24c63420b8caa231d61a4b7220
Zhivolup Andrew <a.zhivolup@norsi-trans.ru>
2024-02-24 18:42:41 +0300

    feat: added new component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import { Slider } from "antd";
 import "./App.scss";
+import { CodesInput } from "./widgets/CodesInput";
 import { Hedgehog } from "./widgets/Hedgehog";
+import { HedgehogSlider } from "./widgets/HedgehogSlider";
 
 function App() {
+  const promoLength = 10;
   return (
     <div className="App">
       <header className="App-header">спаси ежа!</header>
       <Hedgehog />
-      <Slider defaultValue={30} />
+      <HedgehogSlider />
+      <CodesInput promoLength={promoLength} defaultValue="D1SQ8EPDS3" />
     </div>
   );
 }
